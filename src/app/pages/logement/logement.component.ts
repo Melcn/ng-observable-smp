06105438346979475dbe44277971bb9da174ecb7
Melcn <melaniearinosy@outlook.com>
2023-05-17 14:02:30 +0200

    create: module+service+components

---
+++ src/app/pages/logement/logement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logement',
+  templateUrl: './logement.component.html',
+  styleUrls: ['./logement.component.css']
+})
+export class LogementComponent {
+
+}
